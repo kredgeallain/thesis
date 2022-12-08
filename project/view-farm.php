6aeaf1ec543238649db9d67fffc572558dd7c515
kenn <kenn.mark.evangelista@gmail.com>
2022-12-08 12:37:02 +0800

    Update view-farm.php

--- project/view-farm.php
+++ project/view-farm.php
@@ -9,8 +9,12 @@ $mysqli = new mysqli("localhost", $username, $password, $database);
 
 <?php include ("header.php");  ?>
 
-	<section class="view-farm">
-        <h2> Farm Details</h2>
+<div class="wrapper">
+
+<section class="view-user">
+        <h2><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-houses" viewBox="0 0 20 20">
+  <path d="M5.793 1a1 1 0 0 1 1.414 0l.647.646a.5.5 0 1 1-.708.708L6.5 1.707 2 6.207V12.5a.5.5 0 0 0 .5.5.5.5 0 0 1 0 1A1.5 1.5 0 0 1 1 12.5V7.207l-.146.147a.5.5 0 0 1-.708-.708L5.793 1Zm3 1a1 1 0 0 1 1.414 0L12 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l1.854 1.853a.5.5 0 0 1-.708.708L15 8.207V13.5a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 4 13.5V8.207l-.146.147a.5.5 0 1 1-.708-.708L8.793 2Zm.707.707L5 7.207V13.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5V7.207l-4.5-4.5Z"/>
+</svg>Farm Details</h2>
     </section>
 
     <section class="view">
@@ -20,7 +24,7 @@ $mysqli = new mysqli("localhost", $username, $password, $database);
     
 $query = "SELECT * FROM farm";
 
-echo "<b> <center> Farm Details </center> </b> <br> <br>";
+
 
 
 if ($result = $mysqli->query($query)){
@@ -60,13 +64,19 @@ if ($result = $mysqli->query($query)){
             	echo "<td id=' cntct '>" .$row['contactno']. "</td>";
                 echo '<td > 
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" >
-                   <a href= "update-farm.php?farmID='.$row['farmID'].'"> Add Batch </a>
+                   <a href= "update-farm.php?farmID='.$row['farmID'].'"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 20 20">
+                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                   <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                 </svg> Add Batch </a>
                 </button>
                 
                  </td>';
 	        	echo '<td > 
                         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" >
-                           <a href= "update-farm.php?farmID='.$row['farmID'].'"> Edit </a>
+                           <a href= "update-farm.php?farmID='.$row['farmID'].'"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 20 20">
+                           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                           <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                         </svg>  Edit </a>
                         </button>
                         
                 </td>';      
@@ -75,7 +85,10 @@ if ($result = $mysqli->query($query)){
 				echo '
 					<td>
 						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#deleteModal'.$row["farmID"].'">
-							Delete
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 20 20">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                      </svg>Delete
 						</button>
 
 						<!-- Modal -->
@@ -112,27 +125,36 @@ if ($result = $mysqli->query($query)){
 
     <div class="add">
         <div class="add-user-page">
-            <a href="add-user.php">Add User</a>
+            <a href="add-user.php"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 20 20">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>Add Farm</a>
         </div>
     </div>
-
+    </div>
     <!--style-->
 
            <style type="text/css">
+
+        .wrapper{
+            margin-top:10px;
+            border:1px solid grey;
+        }
+
         .view {
             padding: 10px;
-            border: 1px solid #0e2a83;
             margin: 20px;
         }
 
-        .view-farm {
+
+        .view-user {
             display: flex;
-            margin: 20px 10px 5px 20px;
+            margin-top:10px;
             justify-content: center;
         }
 
-        .view-farm h2 {
-            font-size: 40px;
+        .view-user h2 {
+            font-size: 30px;
             font-weight: bold;
         }
 
@@ -143,10 +165,10 @@ if ($result = $mysqli->query($query)){
         }
 
         .add-user-page {
-            background-color: #0d6efd;
-            padding: 10px 40px 10px 40px;
-            margin-top: 30px;
-            margin-right: 100px;
+            background-color:darkblue;
+            padding: 10px 25px 10px 25px;
+            margin-bottom: 30px;
+            margin-right: 80px;
             border-radius: 5px;
         }
 
@@ -155,8 +177,14 @@ if ($result = $mysqli->query($query)){
             color: white;
         }
 
+        .add-user-page:hover {
+            margin-right: 80px;
+            background-color:blue;
+            color: white;
+        }
+
         .add-user-page a:hover {
-            color: #00cc00;
+            color: white;
         }
 
         tr td button a {
@@ -165,14 +193,14 @@ if ($result = $mysqli->query($query)){
         }
 
         a:hover {
-            color: red;
+            color: white;
 
         }
 
         tr:hover {
             background-color: #b0b4b2;
-            
         }
+
         </style>
 </body>
 
