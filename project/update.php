d555486d90dd83844ae2f8a084c9208ac0f98d94
kredgeallain <120442866+helloworldjom@users.noreply.github.com>
2022-12-20 18:08:51 +0800

    .

--- project/update.php
+++ project/update.php
@@ -34,6 +34,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 	$username = $row["username"];
 	$password = $row["password"];
 	$position = $row["position"];
+	$status = $row["status"];
 	$mobile_no = $row["mobile_no"];
 }else{
 	if(isset($_POST['update'])){
@@ -43,9 +44,10 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 		$username = mysqli_real_escape_string($data, $_POST['username']);
 		$password = ($_POST['password']);
 		$position = $_POST["position"];
+		$status = $_POST["status"];
 		$mobile_no = $_POST["mobile_no"];
 	
-		$sql = "UPDATE `user` SET `name`='$name',`username`='$username',`position`='$position',`password`='$password',`mobile_no`='$mobile_no' WHERE userID = '$userID' ";
+		$sql = "UPDATE `user` SET `name`='$name',`username`='$username',`position`='$position',`password`='$password',`status`='$status',`mobile_no`='$mobile_no' WHERE userID = '$userID' ";
 		mysqli_query($data, $sql);
 		header("location:view-user.php");
 		sleep(2);
@@ -61,200 +63,232 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 <?php include ("header.php");  ?>
 
 
-	<section class="log-in">
-		<form action="#" method="post">
-		<div class="text2">
-			<p><b>Update User</b></p>
-		</div>
-	
+<section class="log-in">
+    <form action="#" method="post">
+        <div class="text2">
+            <p><b>Update User</b></p>
+        </div>
+
         <div class="input-wrapper">
 
-        <div class="input">
-				
-			<input type="hidden" name="userID" placeholder="UserID" id="userID" value= "<?php echo $userID; ?>"  >
-		</div>
+            <div class="input">
 
-		<div class="input">
-			<p>Name</p>	
-			<input type="text" name="name" placeholder="Name" id="name" value= "<?php echo $name; ?>" required >
-		</div>
+                <input type="hidden" name="userID" placeholder="UserID" id="userID" value="<?php echo $userID; ?>">
+            </div>
 
-		<div class="inputUser">
-			<p>Username</p>
-			<input type="text" name="username" placeholder="Username" id="username" value= "<?php echo $username; ?>" required>
-		</div>
+            <div class="input">
+                <p>Name</p>
+                <input type="text" name="name" placeholder="Name" id="name" value="<?php echo $name; ?>" required>
+            </div>
 
-		<div class="input3">
-			<p>Password</p>
-			<input type="Password" name="password" placeholder="Password" id="password" value= "<?php echo $password; ?>" required>
-		</div>
+            <div class="inputUser">
+                <p>Username</p>
+                <input type="text" name="username" placeholder="Username" id="username" value="<?php echo $username; ?>"
+                    required>
+            </div>
 
-		<div class="input2">
-			<p>Position</p>
-			<select name="position" id="position" >
-				<option value="admin">Administrator</option>
-				<option value="agent">Agent</option>
-			</select>
-		</div>
-		
-		<div class="input4">
-			<p>Mobile No.</p>
-			<input type="number" name="mobile_no" placeholder="09** *** ****" id="mobile" value= "<?php echo $mobile_no; ?>" required>
-		</div>
+            <div class="input3">
+                <p>Password</p>
+                <input type="Password" name="password" placeholder="Password" id="password"
+                    value="<?php echo $password; ?>" required>
+            </div>
 
-		</div>
+            <div class="input2">
+                <p>Position</p>
+                <select name="position" id="position">
+                    <option value="admin">Administrator</option>
+                    <option value="agent">Agent</option>
+                </select>
+            </div>
+            <div class="input2">
+                <p>Position</p>
+                <select name="status" id="status">
+                    <option value="on">On</option>
+                    <option value="off">Off</option>
+                </select>
+            </div>
 
-		<div class="submit">
+            <div class="input4">
+                <p>Mobile No.</p>
+                <input type="number" name="mobile_no" placeholder="09** *** ****" id="mobile"
+                    value="<?php echo $mobile_no; ?>" required>
+            </div>
 
-			<input type="submit" name = "update" id="submit" value="Update User">
-			
-		</div>
-	</section>
+        </div>
 
-    <!--style-->
+        <div class="submit">
+
+            <input type="submit" name="update" id="submit" value="Update User">
+
+        </div>
+</section>
+
+<!--style-->
 <style type="text/css">
-
-.input-wrapper{
-	display:grid;
-	justify-content:center;
+.input-wrapper {
+    display: grid;
+    justify-content: center;
 }
 
-.log-in{
-	box-shadow: 2px 2px 2px 2px grey;
-	border-radius: 30px;
-	font-family: tahoma;
-	background-color: #f9faff;
-	padding: 15px;
-	margin-top: 30px;
-	margin-right: 300px;
-	margin-left: 300px;
-	margin-bottom: 5px;
-}
-.log-in p{
-	padding: 5px;
-}
-.text2{
-	display: grid;
-	justify-content: center;
-	font-size: 25px;
-}
-.input{
-
-	display: grid;
-
-}
-.input p{
-	margin-top:5px ;
-}
-.inputUser{
-	
-	display: grid;
-
-}
-.inputUser p{
-	margin-top:35px ;
-}
-.input2{
-
-	display: grid;
-	
-}
-.input2 p{
-	margin-top:35px ;
-}
-.input3{
-
-	display: grid;
-	
-}
-.input3 p{
-	margin-top:35px ;
-}
-.input4{
-
-	display: grid;
-
-}
-.input4 p{
-	margin-top:20px ;
-}
-.submit{
-	margin-top:35px;
-	display:grid;
-	justify-content:center;
-	text-align: center;
+.log-in {
+    box-shadow: 2px 2px 2px 2px grey;
+    border-radius: 30px;
+    font-family: tahoma;
+    background-color: #f9faff;
+    padding: 15px;
+    margin-top: 30px;
+    margin-right: 300px;
+    margin-left: 300px;
+    margin-bottom: 5px;
 }
 
-.btn{
-	height: 80%;
-	width: 100%;
-	background-color: #0e2a83;
-	color: white;
-	font-size: 16px;
-	margin: auto;
-	border-radius: 10px;
+.log-in p {
+    padding: 5px;
 }
+
+.text2 {
+    display: grid;
+    justify-content: center;
+    font-size: 25px;
+}
+
+.input {
+
+    display: grid;
+
+}
+
+.input p {
+    margin-top: 5px;
+}
+
+.inputUser {
+
+    display: grid;
+
+}
+
+.inputUser p {
+    margin-top: 35px;
+}
+
+.input2 {
+
+    display: grid;
+
+}
+
+.input2 p {
+    margin-top: 35px;
+}
+
+.input3 {
+
+    display: grid;
+
+}
+
+.input3 p {
+    margin-top: 35px;
+}
+
+.input4 {
+
+    display: grid;
+
+}
+
+.input4 p {
+    margin-top: 20px;
+}
+
+.submit {
+    margin-top: 35px;
+    display: grid;
+    justify-content: center;
+    text-align: center;
+}
+
+.btn {
+    height: 80%;
+    width: 100%;
+    background-color: #0e2a83;
+    color: white;
+    font-size: 16px;
+    margin: auto;
+    border-radius: 10px;
+}
+
 input[type=submit] {
-	border-radius: 10px;
-  background-color: #0e2a83;
-  border: none;
-  color: white;
-  padding: 16px 32px;
-  text-decoration: none;
-  margin: 4px 2px;
-  cursor: pointer;
+    border-radius: 10px;
+    background-color: #0e2a83;
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-decoration: none;
+    margin: 4px 2px;
+    cursor: pointer;
 }
+
 input {
-	margin-top:-15px;
-	color: black;
-	border-radius: 10px;
-	padding: 10px;
+    margin-top: -15px;
+    color: black;
+    border-radius: 10px;
+    padding: 10px;
 
 }
+
 select {
-	margin-top:-15px;
-	padding: 10px;
+    margin-top: -15px;
+    padding: 10px;
 
     outline: 0;
     background-image: none;
     border: 1px solid black;
-    border-radius:10px ;
+    border-radius: 10px;
 }
+
 summary {
-	cursor: pointer;
-	list-style: none;
+    cursor: pointer;
+    list-style: none;
 }
-.drop-menu a{
-	text-decoration: none;
-	color: white;
+
+.drop-menu a {
+    text-decoration: none;
+    color: white;
 }
+
 ul {
-	border-radius: 15px;
-	box-shadow: 2px, 2px, 2px,2px black;
-	background-color: grey;
-	padding-bottom: 10px;
-	margin-top: -3px;
-	margin-left: -210px;
-	margin-right: 50px;
-	margin-bottom: -203px;
+    border-radius: 15px;
+    box-shadow: 2px, 2px, 2px, 2px black;
+    background-color: grey;
+    padding-bottom: 10px;
+    margin-top: -3px;
+    margin-left: -210px;
+    margin-right: 50px;
+    margin-bottom: -203px;
 }
+
 li {
-	padding-bottom: 10px;
-	padding-top: 20px;
-	padding-left:10px;
-	padding-right:20px;
-	list-style: none;
+    padding-bottom: 10px;
+    padding-top: 20px;
+    padding-left: 10px;
+    padding-right: 20px;
+    list-style: none;
 }
+
 .side-menu {
-	padding-right: 30px;
- 	display: flex;
- 	justify-content: flex-end;
- 	margin-top: -90px;
- 	margin-bottom: 20px;
- }
-.side-menu img{
- 	margin-left: -165px;
- }
+    padding-right: 30px;
+    display: flex;
+    justify-content: flex-end;
+    margin-top: -90px;
+    margin-bottom: 20px;
+}
+
+.side-menu img {
+    margin-left: -165px;
+}
 </style>
 </body>
+
 </html>
--- project/view-user.php
+++ project/view-user.php
@@ -14,10 +14,12 @@ $mysqli = new mysqli("localhost", $username, $password, $database);
 <section class="wrapper">
 
     <section class="view-user">
-        <h2><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-workspace" viewBox="0 0 16 20">
-  <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-  <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z"/>
-</svg>Users</h2>
+        <h2><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                class="bi bi-person-workspace" viewBox="0 0 16 20">
+                <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                <path
+                    d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z" />
+            </svg>Users</h2>
     </section>
 
     <section class="view">
@@ -35,6 +37,7 @@ if ($result = $mysqli->query($query)){
 				<th scope='col' hidden id='count'>UserID</th>
 				<th scope='col' id='name'>Name</th>
 				<th scope='col' id='u-name'>Username</th>
+                <th scope='col' id='brgy'>Address</th>
 				<th scope='col' id='pos'>Position</th>
 				<th scope='col' id='cntct'>Contact No</th>
 				<th scope='col' id='edit'> Edit </th>
@@ -47,6 +50,7 @@ if ($result = $mysqli->query($query)){
         while ($row = $result->fetch_assoc()) {
             $userID = $row["userID"];
             $name = $row["name"];
+            $brgy = $row["baranggay"];
             $username = $row["username"];
             $position = $row["position"];       
             $mobile_no = $row["mobile_no"];
@@ -56,6 +60,7 @@ if ($result = $mysqli->query($query)){
             	echo "<td hidden >" .$row['userID']. "</td>";
             	echo "<td id='name'>" .$row['name']. "</td>";
             	echo "<td id='u-name'>" .$row['username']. "</td>";
+                echo "<td id='brgy'>" .$row['baranggay']. "</td>";
             	echo "<td id='pos'>" .$row['position']. "</td>";
 	        	echo "<td id='cntct'>" .$row['mobile_no']. "</td>";				
             	echo '<td > 
@@ -116,95 +121,95 @@ if ($result = $mysqli->query($query)){
 
     <div class="add">
         <div class="add-user-page">
-            <a href="add-user.php"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 20 20">
-  <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-  <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-</svg>Add User</a>
+            <a href="add-user.php"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                    class="bi bi-person-plus" viewBox="0 0 20 20">
+                    <path
+                        d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                    <path fill-rule="evenodd"
+                        d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                </svg>Add User</a>
         </div>
     </div>
 
-    </section>
-    <!--style-->
+</section>
+<!--style-->
 
-        <style type="text/css">
+<style type="text/css">
+.wrapper {
+    margin-top: 10px;
+    border: 1px solid grey;
+}
 
-        .wrapper{
-            margin-top:10px;
-            border:1px solid grey;
-        }
+.view {
+    padding-top: 10px;
+    margin: 10px;
+}
 
-        .view {
-            padding-top:10px ;
-            margin: 10px;
-        }
+.view-user {
+    display: flex;
+    margin-top: 10px;
+    justify-content: center;
+}
 
-        .view-user {
-            display: flex;
-            margin-top:10px;
-            justify-content: center;
-        }
+.view-user h2 {
+    font-size: 30px;
+    font-weight: bold;
+}
 
-        .view-user h2 {
-            font-size: 30px;
-            font-weight: bold;
-        }
+.add {
 
-        .add {
+    display: flex;
+    justify-content: flex-end;
+}
 
-            display: flex;
-            justify-content: flex-end;
-        }
+.add-user-page {
+    background-color: darkblue;
+    padding: 10px 25px 10px 25px;
+    margin-bottom: 30px;
+    margin-right: 80px;
+    border-radius: 5px;
+}
 
-        .add-user-page {
-            background-color:darkblue;
-            padding: 10px 25px 10px 25px;
-            margin-bottom: 30px;
-            margin-right: 80px;
-            border-radius: 5px;
-        }
+.add-user-page a {
+    text-decoration: none;
+    color: white;
+}
 
-        .add-user-page a {
-            text-decoration: none;
-            color: white;
-        }
+.add-user-page:hover {
+    margin-right: 80px;
+    background-color: blue;
+    color: white;
+}
 
-        .add-user-page:hover {
-            margin-right: 80px;
-            background-color:blue;
-            color: white;
-        }
+.add-user-page a:hover {
+    color: white;
+}
 
-        .add-user-page a:hover {
-            color: white;
-        }
+tr td button a {
+    text-decoration: none;
+    color: white;
+}
 
-        tr td button a {
-            text-decoration: none;
-            color: white;
-        }
+a:hover {
+    color: white;
 
-        a:hover {
-            color: white;
+}
 
-        }
+tr:hover {
+    background-color: #b0b4b2;
+}
 
-        tr:hover {
-            background-color: #b0b4b2;
-        }
+.delete-button button {
+    border: none;
+    background-color: red;
+}
 
-        .delete-button button{
-            border:none;
-            background-color:red;
-        }
-
-        .delete-button button:hover{
-            border:none;
-            background-color:darkred;
-            color:white;
-        }
-
-
-        </style>
+.delete-button button:hover {
+    border: none;
+    background-color: darkred;
+    color: white;
+}
+</style>
 </body>
 
 </html>
