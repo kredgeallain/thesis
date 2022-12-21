60a02ac136694daabae6a9f1f18e56ff17d2d28b
helloworldJOM <120442866+helloworldjom@users.noreply.github.com>
2022-12-21 12:57:44 +0800

    .

--- project/header.php
+++ project/header.php
@@ -385,9 +385,9 @@
             </b>
         </div>
 
-        <a href="#home"><img src="../image/view-data.png" alt="view record icon" width="30px"> View Records</a>
+        <a href="try1.php"><img src="../image/view-data.png" alt="view record icon" width="30px"> View Records</a>
 
-        <a href="try1.php"><img src="../image/generate-report1.png" alt="generate report icon" width="25px"> Generate
+        <a href="view-layer.php"><img src="../image/generate-report1.png" alt="generate report icon" width="25px"> Generate
             reports</a>
 
         <a href="record1.php"><img src="../image/record-icon.png" alt="generate report icon" width="30px"> Record Reports</a>
