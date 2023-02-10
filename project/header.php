fdd1d50b92da451b683197f6468dd77b371e3174
kenn <kenn.mark.evangelista@gmail.com>
2023-02-11 01:16:55 +0800

    Update header.php

--- project/header.php
+++ project/header.php
@@ -513,9 +513,14 @@ li{
             </b>
         </div>
 
-        <a href="add-farm-batch.php"><img src="../image/view-data.png" alt="view record icon" width="30px">Add Batch</a>
+        <a href="add-farm-batch.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 20 20">
+        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+        </svg>Add Batch</a>
 
-        <a href="view-batches.php"><img src="../image/generate-report1.png" alt="generate report icon" width="25px">View Batches</a>
+        <a href="view-batches.php"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 20 20">
+  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+</svg>View Batches</a>
 
   
         <!--user-->
