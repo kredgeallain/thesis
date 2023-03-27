9f5d7a406f363155f7862bdfcfe3b32836bdc168
helloworldJOM <120442866+helloworldjom@users.noreply.github.com>
2023-03-27 16:10:26 +0800

    no delete

--- project/view-user.php
+++ project/view-user.php
@@ -168,43 +168,8 @@ if ($result = $conn->query($sql)){
                                     </div>
                                     </div>
 
-                        
-                </td>     
-                    
-				
-				
-					<td>
-                    <div class="delete-button">
-						<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#deleteModal'.$row["userID"].'">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 20 20">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                      </svg>Delete
-						</button>
-                    </div>
-
-						<!-- Modal -->
-						<form action="view-user.php" method="GET">
-							<div class="modal fade" id="deleteModal'.$row["userID"].'" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
-								<div class="modal-dialog">
-									<div class="modal-content">
-									<div class="modal-header">
-										<h1 class="modal-title fs-5" id="exampleModalLabel">Confirm</h1>
-										<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-									</div>
-									<div class="modal-body">
-                                        <center> Are you sure you want to delete the user with this id?? </center>										
-									</div>									
-                                        <center> <p> '.$row["userID"].' </p> </center>
-									<div class="modal-footer">
-										<button type="button" class="btn btn-secondary" data-bs-dismiss="modal"> Cancel </button>
-										<button id="delete-btn" type="submit" class="btn btn-primary"> <a id="delete-yes" href="delete.php?id='.$row["userID"].'"> Yes </a> </button>
-									</div>
-									</div>
-								</div>
-							</div>
-						</form>
-					</td>';
+                   
+					</td>';  
 			echo"</tr>";
 		}
 	"</table>";
