543c70ce023c7acc3e3d5cb2e361a8d667a9ba76
kin109 <kenn.mark.evangelista@gmail.com>
2023-04-10 22:45:23 +0800

    Update homepage.php

--- project/user ui/homepage.php
+++ project/user ui/homepage.php
@@ -173,7 +173,7 @@ $row = $result->fetch_assoc();
                 <div class="brgy-name">
                   <h4><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-</svg>View Poultry Batches</h4>
+</svg>Poultry Batches</h4>
                 </div>
                 <div class="brgy-nxt">
                 <a href="view-batches.php"><h9>〉</h9></a>
@@ -190,7 +190,7 @@ $row = $result->fetch_assoc();
 </svg>Record Productions</h4>
                 </div>
                 <div class="brgy-nxt">
-                <a href="record-home.php"><h9>〉</h9></a>
+                <a href="record.php"><h9>〉</h9></a>
                 </div>
               </div>
             </section>
